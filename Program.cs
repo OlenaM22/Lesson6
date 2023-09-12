ee46b70420970a4927e9701a19f7039df5d386ab
OlenaM22 <143819929+OlenaM22@users.noreply.github.com>
2023-09-12 19:59:40 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(MaxValue(1, 2, 3, 4, 5));  
+        Console.WriteLine(MaxValue(10, 20, 30));     
+    }
+
+    public static int MaxValue(params int[] numbers)
+    {
+        return numbers.Max();
+    }
+}
